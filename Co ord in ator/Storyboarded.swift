6c82d953ecf482d74c7f7a2819c258934a197ab4
Wissa Azmy <wissa.azmy@gmail.com>
2019-06-01 13:47:33 +0200

    Initialize app navigation manually in the AppDelegate

--- Co ord in ator/Storyboarded.swift
+++ Co ord in ator/Storyboarded.swift
@@ -15,6 +15,7 @@ protocol Storyboarded {
 
 extension Storyboarded where Self: UIViewController {
     static func instantiate() -> Self {
+        // Must add the StoryboardID the same as the controller name in the identity inspector for it to work
         let id = String(describing: self)
         let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
         return storyboard.instantiateViewController(withIdentifier: id) as! Self

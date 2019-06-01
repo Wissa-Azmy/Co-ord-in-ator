3c70e2da97add20e14cee3c10034b2f0619dbd9a
Wissa Azmy <wissa.azmy@gmail.com>
2019-06-01 13:51:59 +0200

    Add Buy & CreateAccount view controllers

---
+++ Co ord in ator/BuyViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BuyViewController.swift
+//  Co ord in ator
+//
+//  Created by Wissa Azmy on 6/1/19.
+//  Copyright © 2019 Wissa Azmy. All rights reserved.
+//
+
+import UIKit
+
+class BuyViewController: UIViewController, Storyboarded {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
